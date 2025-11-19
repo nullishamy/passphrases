7dd82f895a53f08c7a58ee000c1c2be01e2ecebb
nullishamy <git@amyerskine.me>
2025-11-19 11:49:42 +0000

    History

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ const ALL_WORDS = [
 const $$ = document.querySelector.bind(document)
 const $ = document.querySelectorAll.bind(document)
 
-
 console.log("working with", ALL_WORDS.length, "words")
+
+const MAX_HISTORY = 5
+const HISTORY_KEY = "generatorHistory"
+let generatorHistory = JSON.parse(localStorage.getItem(HISTORY_KEY) ?? "[]")
 
 const wordCount = $$("#word-count")
 const wordCountValue = $$("#word-count-value")
@@ -34,6 +37,21 @@ function capitalizeFirstLetter(val) {
   return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
 
+function showGeneratorHistory() {
+  if (generatorHistory.length > MAX_HISTORY) {
+    generatorHistory = generatorHistory.slice(0, 5)
+  }
+  for (let i = 0; i < MAX_HISTORY; i++) {
+    const item = generatorHistory[i]
+    if (item == undefined) {
+      continue
+    }
+
+    $$(`#history-${i}`).textContent = item.phrase
+  }
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(generatorHistory))
+}
+
 function generate(config) {
   const output = $$("#output")
 
@@ -48,7 +66,6 @@ function generate(config) {
     "dash": "-"
   }
 
-
   if (config.capitalisation === "word") {
     words = words.map(w => capitalizeFirstLetter(w))
   } else if (config.capitalisation === "first") {
@@ -58,11 +75,17 @@ function generate(config) {
   const out = words.join(separators[config.separator])
   console.log("generated", out, "with config", config)
   output.textContent = out
+
+  generatorHistory = [{ phrase: out, config }, ...generatorHistory]
+  showGeneratorHistory()
 }
 
 function setFormFromParms() {
   const searchParams = new URLSearchParams(window.location.search)
-  const hasAll = ["separator", "capitalisation", "word-count"].every(k => searchParams.has(k))
+  const hasAll = ["separator", "capitalisation", "word-count"].every(k => {
+    const v = searchParams.get(k)
+    return v != null && v !== "null"
+  })
 
   if (!hasAll) {
     return
@@ -93,6 +116,16 @@ form.addEventListener("submit", e => {
   e.preventDefault()
 
   const data = new FormData(e.target)
+  const hasAll = ["separator", "capitalisation", "word-count"].every(k => {
+    const v = data.get(k)
+    return v != null && v !== "null"
+  })
+  if (!hasAll) {
+    const output = $$("#output")
+    output.textContent = "missing fields in form"
+    return false
+  }
+  
   setParams(data)
 
   console.log("generating...", data)
@@ -114,5 +147,6 @@ $(".clip-btn").forEach(btn => {
   })
 });
 
-new ClipboardJS('.clip-btn');
+new ClipboardJS('.clip-btn')
 setFormFromParms()
+showGeneratorHistory()
